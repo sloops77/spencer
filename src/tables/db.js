73865a11e5dcbb451c17c970f66e408fd2697605
arolave <andres@thinkingbytes.net>
2019-10-08 10:43:43 +0300

    fixes

--- src/tables/db.js
+++ src/tables/db.js
@@ -2,14 +2,15 @@ const _ = require("lodash/fp");
 const knex = require("./index");
 
 async function createSchema({ schemaName = "public", softDelete = false, tableCreators = [] } = {}) {
-  await knex.raw(`DROP SCHEMA IF EXISTS "${schemaName}" CASCADE`);
-  await knex.raw(`CREATE SCHEMA "${schemaName}"`);
+  await knex.raw(`CREATE SCHEMA IF NOT EXISTS "${schemaName}"`);
 
   if (softDelete) {
-    await knex.raw(`DROP SCHEMA IF EXISTS ${schemaName}_deleted CASCADE;`);
-    await knex.raw(`CREATE SCHEMA ${schemaName}_deleted;`);
-    await knex.raw(`DROP SCHEMA IF EXISTS ${schemaName}_combined CASCADE;`);
-    await knex.raw(`CREATE SCHEMA ${schemaName}_combined;`);
+    await knex.raw(`CREATE SCHEMA IF NOT EXISTS ${schemaName}_deleted;`);
+    await knex.raw(`CREATE SCHEMA IF NOT EXISTS ${schemaName}_combined;`);
+  }
+
+  if (_.isEmpty(tableCreators)) {
+    return;
   }
 
   const schemaBuilder = knex.schema.withSchema([schemaName]);
