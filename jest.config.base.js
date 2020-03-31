4f605e275062e23c9ee1cd9885739bf23362f95d
arolave <andres@thinkingbytes.net>
2020-03-31 10:33:06 +0300

    fix config

--- jest.config.base.js
+++ jest.config.base.js
@@ -6,11 +6,6 @@ module.exports = {
   testRegex: "(/test/.*.(test|spec)).(jsx?|tsx?)$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   collectCoverage: true,
-  collectCoverageFrom: [
-    "src/**/*.{js,jsx}",
-    "!src/index.js",
-    "!test/**",
-    "!node_modules/**"
-  ],
+  collectCoverageFrom: ["src/**/*.{js,jsx}", "!src/index.js", "!test/**", "!node_modules/**"],
   verbose: true,
 };
