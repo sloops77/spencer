167234ee3e25d9c2bee84148647ca6c659b43012
arolave <andres@thinkingbytes.net>
2020-04-16 13:17:31 +0300

    reintroduce the @spencejs/spence package

---
+++ packages/spence/src/index.js
@@ -0,0 +1,7 @@
+/* eslint-disable global-require */
+module.exports = {
+  ...require("@spencejs/spence-api"),
+  ...require("@spencejs/spence-core"),
+  ...require("@spencejs/spence-events"),
+  ...require("@spencejs/spence-tables"),
+};
