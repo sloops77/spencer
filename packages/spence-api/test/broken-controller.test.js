ad6734f0f85a9c67abaa66081525fb1cef254bb4
Andres Olave <arolave@gmail.com>
2026-04-09 07:21:49 +1000

    test: cover broken rest handler configs
    Signed-off-by: Andres Olave <arolave@gmail.com>

--- packages/spence-api/test/broken-controller.test.js
+++ packages/spence-api/test/broken-controller.test.js
@@ -5,7 +5,7 @@ const { reposPlugin } = require("@spencejs/spence-pg-repos");
 const RestConfigurationError = require("../src/rest/RestConfigurationError");
 const initController = require("../src/rest/controller");
 const fastifyRest = require("../src/rest/plugin");
-const { update } = require("../src/rest/rest-handlers");
+const { create, getById, getAll, update } = require("../src/rest/rest-handlers");
 const { simpleController } = require("./helpers/pg-rest-controller");
 
 function pluginNotRegisteredFastify(routes) {
@@ -20,6 +20,29 @@ function pluginNotRegisteredFastify(routes) {
   return app;
 }
 
+function buildBrokenController(route, schemas) {
+  return initController(
+    {
+      tag: "examples",
+      schemas,
+      tableName: "examples",
+    },
+    (router, controllerOptions, next) => {
+      router.restRoutes(route);
+      next();
+    },
+  );
+}
+
+function brokenControllerFastify(route, schemas) {
+  const app = fastifyFactory();
+
+  app.register(fastifyRest);
+  app.register(buildBrokenController(route, schemas), { prefix: "/examples" });
+
+  return app;
+}
+
 describe("controller plugin errors", () => {
   it("should error if plugin wasnt defined", async () => {
     const app = pluginNotRegisteredFastify({ "/examples": simpleController }, {});
@@ -28,22 +51,32 @@ describe("controller plugin errors", () => {
     );
   });
 
-  it("should error if update route is defined without an update schema", async () => {
-    const brokenUpdateController = initController(
-      {
-        tag: "examples",
-        schemas: { create: null, update: null, reply: null },
-        tableName: "examples",
-      },
-      (router, controllerOptions, next) => {
-        router.restRoutes(update);
-        next();
-      },
-    );
-    const app = fastifyFactory();
+  it("should error if create route is defined without a create schema", async () => {
+    const app = brokenControllerFastify(create, { create: null, update: null, reply: null });
 
-    app.register(fastifyRest);
-    app.register(brokenUpdateController, { prefix: "/examples" });
+    await expect(app.ready()).rejects.toEqual(
+      new RestConfigurationError("Must specify a create and reply schema when a create route is specified"),
+    );
+  });
+
+  it("should error if getById route is defined without a reply schema", async () => {
+    const app = brokenControllerFastify(getById, { create: null, update: null, reply: null });
+
+    await expect(app.ready()).rejects.toEqual(
+      new RestConfigurationError("Must specify a reply schema when a getById route is specified"),
+    );
+  });
+
+  it("should error if getAll route is defined without a reply schema", async () => {
+    const app = brokenControllerFastify(getAll, { create: null, update: null, reply: null });
+
+    await expect(app.ready()).rejects.toEqual(
+      new RestConfigurationError("Must specify a reply schema when a getAll route is specified"),
+    );
+  });
+
+  it("should error if update route is defined without an update schema", async () => {
+    const app = brokenControllerFastify(update, { create: null, update: null, reply: null });
 
     await expect(app.ready()).rejects.toEqual(
       new RestConfigurationError("Must specify an update and reply schema when an update route is specified"),
