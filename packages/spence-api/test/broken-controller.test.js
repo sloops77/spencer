be5bc37903d67c5337c51c6f3e8c879a735726e5
Andres Olave <arolave@gmail.com>
2026-04-09 07:11:17 +1000

    test: cover update schema guard
    Signed-off-by: Andres Olave <arolave@gmail.com>

--- packages/spence-api/test/broken-controller.test.js
+++ packages/spence-api/test/broken-controller.test.js
@@ -3,6 +3,7 @@ const _ = require("lodash/fp");
 
 const { reposPlugin } = require("@spencejs/spence-pg-repos");
 const RestConfigurationError = require("../src/rest/RestConfigurationError");
+const { update } = require("../src/rest/rest-handlers");
 const { simpleController } = require("./helpers/pg-rest-controller");
 
 function pluginNotRegisteredFastify(routes) {
@@ -22,6 +23,17 @@ describe("controller plugin errors", () => {
     const app = pluginNotRegisteredFastify({ "/examples": simpleController }, {});
     await expect(app.ready()).rejects.toEqual(
       new RestConfigurationError("To use @spence, please register the fastifyRest plugin with the fastify server"),
+    );
+  });
+
+  it("should error if update route is defined without an update schema", async () => {
+    expect(() =>
+      update.schema({
+        schemas: { updateSchema: null, replySchema: null },
+        schemaBuilders: { updateOne: _.noop },
+      }),
+    ).toThrow(
+      new RestConfigurationError("Must specify an update and reply schema when an update route is specified"),
     );
   });
 });
